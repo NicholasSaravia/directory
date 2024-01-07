e157dbc2ef11a5912c08a3943e15c3cd7b72d672
NicholasSaravia <nick.saravia@gmail.com>
2024-01-07 17:41:46 -0500

    add secret code check using server action

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} h-[100dvh] w-[100dvw]`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
 "use client";
-import { signInWithEmail } from "@/lib/api/auth";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { signIn } from "@/app/actions";
 
 export default function Home() {
   const [email, setEmail] = useState("");
+  const [secretCode, setSecretCode] = useState("");
+  const [loading, setLoading] = useState(false);
   const [signUpStatus, setSignUpStatus] = useState<
     "ERROR" | "SUCCESS" | "DEFAULT"
   >("DEFAULT");
 
   return (
-    <main className="h-[100dvh] w-[100dvw]">
+    <main className="flex flex-col h-full justify-center items-center">
       <form
+        className="flex flex-col gap-2 max-w-60 border p-4 rounded-lg"
         onSubmit={async (e) => {
           e.preventDefault();
-          const { data, error } = await signInWithEmail(email);
+          setLoading(true);
+
+          const redirectUrl = new URL("/directory", window.location.href).href;
+          const { error } = await signIn(email, secretCode, redirectUrl);
 
           if (error) {
             setSignUpStatus("ERROR");
@@ -25,26 +31,33 @@ export default function Home() {
             console.log("resetting");
             setSignUpStatus("DEFAULT");
           }, 3000);
+
+          setLoading(false);
         }}
       >
-        <>
-          <input
-            type="email"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button>Sign In via magic link</button>
-        </>
-        {signUpStatus === "ERROR" && (
-          <div>Something went wrong submitting your request</div>
-        )}
-        {signUpStatus === "SUCCESS" && (
-          <div>
-            Check your email for a magic link to sign in. If you don&apos;t see
-            it, check your spam folder.
-          </div>
-        )}
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Secret Code"
+          onChange={(e) => setSecretCode(e.target.value)}
+        />
+        <button className="btn-primary" disabled={loading}>
+          Sign In via magic link
+        </button>
       </form>
+      {signUpStatus === "ERROR" && (
+        <div>Something went wrong submitting your request</div>
+      )}
+      {signUpStatus === "SUCCESS" && (
+        <div>
+          Check your email for a magic link to sign in. If you don&apos;t see
+          it, check your spam folder.
+        </div>
+      )}
     </main>
   );
 }
